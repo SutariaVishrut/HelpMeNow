e4f81d1c58f31c8e81a813aeb212ab5742c24f69
vsutaria <v18kingmj@gmail.com>
2023-04-27 15:39:40 -0400

    Google map integration (#4)
    * Data models for backend
    
    * quick fix
    
    * comment fix
    
    * Google maps integrated
    
    ---------
    
    Co-authored-by: Vishrut Sutaria <sutariavishrut@gmail.com>
    Co-authored-by: SutariaVishrut <128194218+SutariaVishrut@users.noreply.github.com>

--- app/src/main/java/com/example/helpmenow/MainActivity.kt
+++ app/src/main/java/com/example/helpmenow/MainActivity.kt
@@ -11,15 +11,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.helpmenow.ui.theme.HelpMeNowTheme
+import com.example.helpmenow.ui.theme.ShowMap
+import com.google.android.gms.maps.model.LatLng
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val singapore = LatLng(1.35, 103.87)
         setContent {
             HelpMeNowTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    ShowMap(singapore)
                 }
             }
         }
